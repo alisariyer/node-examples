59703fa99d8a8c53acddb9b02f230a4222ef3740
Ali Sariyer <alisariyer@gmail.com>
2023-03-27 02:00:26 +0200

    Add static asset and use path

--- template-ejs/app.js
+++ template-ejs/app.js
@@ -3,11 +3,15 @@ const app = express();
 const port = 8080;
 const path = require('path');
 const redditData = require('./data.json');
-console.log(redditData);
 
 // Define template engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
+
+// Static route (You can define multiple as js, css etc. but now only public)
+// app.use(express.static('public')); we comment it because it does not work in other directories
+// with path.join(__dirname, path) we get absolute path of the current file
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Define router
 const indexRouter = require('./routes/index');
