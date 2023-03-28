d1842bae2c30143c3e94416b53ce5b914a895f4b
Ali Sariyer <alisariyer@gmail.com>
2023-03-28 02:16:21 +0200

    Setup view engine ejs

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response");
@@ -16,3 +20,19 @@ app.post('/tacos', (req, res) => {
 app.listen(3000, (req, res) => {
     console.log('Listening on port 3000');
 })
+
+// GET /comments                : list all comments
+// POST /comments             : create a new comment
+// GET /comments/:id         : get one comment using id
+// PATCH /comments/:id    : update one comment
+// DELETE /comments/:id  : destroy one comment
+
+// Note from the course
+// NAME    PATH                VERB      PURPOSE
+// Index   /comments           GET       Display all comments
+// New     /comments/new       GET       Form to create new comment
+// Create  /comments           POST      Creates new comment on server
+// Show    /comments/:id       GET       Details for one specific comment
+// Edit    /comments/:id/edit  GET       Form to edit specific comment
+// Update  /comments/:id       PATCH
+// Destroy /comments/:id       DELETE
