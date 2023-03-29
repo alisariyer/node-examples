f4f073a8b397c3aeb1e5eb350eb9d4116ba8ecb1
Ali Sariyer <alisariyer@gmail.com>
2023-03-29 02:16:17 +0200

    Use uuid, setup DELETE, UPDATE request options

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -1,46 +1,51 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 app.use(express.json());
 
+// Override with POST having ?_method=DELETE or other verbs (_method can be another string)
+app.use(methodOverride('_method'));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-const comments = [
+let comments = [
     {
-        id: 1,
+        id: uuid(),
         username: 'Felix',
         comment: 'Hey, this is so funny!'
     },
     {
-        id: 2,
+        id: uuid(),
         username: 'Adam',
         comment: 'Cute cat, white and black harmony!'
     },
     {
-        id: 3,
+        id: uuid(),
         username: 'Selim',
         comment: 'Your app is very effective.'
     },
     {
-        id: 4,
+        id: uuid(),
         username: 'Canan',
         comment: 'Can you make more than that, more complicated?'
     },
     {
-        id: 5,
+        id: uuid(),
         username: 'Danny',
         comment: 'I am just sleeping, please do not interrupt me!'
     },
     {
-        id: 6,
+        id: uuid(),
         username: 'Said',
         comment: 'Read more and get lightener'
     },
     {
-        id: 7,
+        id: uuid(),
         username: 'Will',
         comment: 'No time, no money. Man what\'s up?'
     }
@@ -59,16 +64,41 @@ app.get('/comments/new', (req, res) => {
 // Create REST pattern: create a new comment
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body;
-    comments.push({ username, comment });
+    comments.push({ id: uuid(), username, comment });
     res.redirect('/comments');
 })
 
 // Show REST pattern: show a unique comment
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
-    const comment = comments.find(c => c.id === parseInt(id));
+    const comment = comments.find(c => c.id === id);
     res.render('comments/show', { comment });
 });
+
+// Edit REST pattern: edit a comment to use with next PATCH request
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment });
+})
+
+// Update REST pattern: update a comment
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newComment = req.body.comment;
+    const targetComment = comments.find(c => c.id === id);
+    targetComment.comment = newComment;
+    res.redirect('/comments');
+});
+
+// Destroy REST pattern: delete a comment
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    // This is not good idea normally muted arrays but temporarly
+    // Because filter creates a new array
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
+})
 
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response");
