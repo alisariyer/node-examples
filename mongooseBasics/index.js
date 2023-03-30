23997470f5ae7381cffffdd87e72fd27ed69206e
Ali Sariyer <alisariyer@gmail.com>
2023-03-30 23:55:45 +0200

    Change mongodb adress, add new Schema and model

--- mongooseBasics/index.js
+++ mongooseBasics/index.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test')
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+const { Schema, model } = mongoose;
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/test');
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
     console.log('Connection established!');
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 main().catch(err => console.log(err));
+
+const movieSchema = new Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = model('Movie', movieSchema);
+
+const avatar = new Movie({ title: 'Avatar', year: 2022, score: 7.8, rating: "R" })
+
+console.log(avatar);
