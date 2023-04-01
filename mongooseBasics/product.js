47b37e318348a23f41c2dc2bc626650bae883497
Ali Sariyer <alisariyer@gmail.com>
2023-04-01 17:30:30 +0200

    Update

--- mongooseBasics/product.js
+++ mongooseBasics/product.js
@@ -17,7 +17,7 @@ const productSchema = new Schema({
     price: { 
             type: Number,
             required: true,
-            min: 0
+            min: [0, 'Price must be positive!']
          },
     onSale: {
             type: Boolean,
@@ -33,17 +33,56 @@ const productSchema = new Schema({
             type: Number,
             default: 0
         }
+    },
+    size: {
+        type: String,
+        enum: ['XS', 'S', 'M', 'L', 'XL']
     }
 })
 
-const Product = model('Product', productSchema);
+// const Product = model('Product', productSchema);
 
-// const bike = new Product({ name: 'Tire pump', price: 25, categories: ['Cycling'] });
+// const bike = new Product({ name: 'Cycling Jersey', price: 45, categories: ['Cycling'], size: 'XS' });
 
 // bike.save()
 //     .then(data => console.log('Saved: ', data))
-//     .catch(err => console.log('Error: ', err));
+//     .catch(err => console.log('Error: ', err.errors));
 
-Product.findOneAndUpdate({ name: 'Tire pump'}, { price: -30}, { new: true, runValidators: true})
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+// Product.findOneAndUpdate({ name: 'Tire pump'}, { price: -30}, { new: true, runValidators: true})
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err.errors.price.properties.message));
+
+// productSchema.methods.greet = function () {
+//     console.log('Hellooooo');
+//     console.log(` from ${this.name}`)
+// }
+
+productSchema.methods.toggleOnSale = function () {
+    this.onSale = !this.onSale;
+    return this.save();
+}
+
+productSchema.methods.addCategory = function(newCategory) {
+    this.categories.push(newCategory);
+    return this.save();
+}
+
+productSchema.statics.fireSale = function() {
+    return this.updateMany({}, { onSale: true, price: 0});
+}
+
+const Product = model('Product', productSchema);
+
+const findProduct = async () => {
+    const foundProduct = await Product.findOne({ name: 'Mountain bike'});
+    // foundProduct.greet();
+    console.log(foundProduct);
+    await foundProduct.toggleOnSale();
+    console.log(foundProduct);
+    await foundProduct.addCategory('Outdoors');
+    console.log(foundProduct);
+}
+
+// findProduct();
+
+Product.fireSale().then(data => console.log(data));
