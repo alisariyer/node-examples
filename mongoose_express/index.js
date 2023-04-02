5a4385450fd9aaacb9c4e3e303c7a5a290d43c88
Ali Sariyer <alisariyer@gmail.com>
2023-04-02 16:40:33 +0200

    Basic mongoose setup

--- mongoose_express/index.js
+++ mongoose_express/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+const main = async () => {
+    await mongoose.connect('mongodb://127.0.0.1:27017/testdb');
+};
+
+main()
+    .then(msg => console.log('Connection open'))
+    .catch(err => console.log('DB Error: ', err));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Homepage');
+})
+
+app.get('/dog', (req, res) => {
+    res.send('Sisst');
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
